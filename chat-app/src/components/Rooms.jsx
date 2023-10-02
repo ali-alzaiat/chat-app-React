61cce9802c23562c8d28597045c33784e6cc3f5c
ali-alzaiat <alialzaiat2000@gmail.com>
2023-10-03 00:16:50 +0300

    added scroller to the sidebar

--- chat-app/src/components/Rooms.jsx
+++ chat-app/src/components/Rooms.jsx
@@ -43,6 +43,27 @@ function Rooms() {
                     <p className="last-message">Hello</p>
                 </div>
             </div>
+            <div className="chat">
+                <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
+                <div className="chat-info">
+                    <span className="userName">Ali</span>
+                    <p className="last-message">Hello</p>
+                </div>
+            </div>
+            <div className="chat">
+                <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
+                <div className="chat-info">
+                    <span className="userName">Ali</span>
+                    <p className="last-message">Hello</p>
+                </div>
+            </div>
+            <div className="chat">
+                <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
+                <div className="chat-info">
+                    <span className="userName">Ali</span>
+                    <p className="last-message">Hello</p>
+                </div>
+            </div>
         </div>
      );
 }
