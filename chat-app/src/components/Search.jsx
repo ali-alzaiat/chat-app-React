e48ffca1ac40ecc3ec6e91732972338f3f6ffd1a
ali-alzaiat <alialzaiat2000@gmail.com>
2023-10-02 22:12:13 +0300

    added search to the sidebar

--- chat-app/src/components/Search.jsx
+++ chat-app/src/components/Search.jsx
@@ -1,7 +1,13 @@
 function Search() {
     return ( 
         <div className="search">
-
+            <div className="input-form">
+                <input type="text" name="search" id="search" placeholder="Search for a user" />
+            </div>
+            <div className="result">
+                <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
+                <span className="userName">Ali</span>
+            </div>
         </div>
      );
 }
