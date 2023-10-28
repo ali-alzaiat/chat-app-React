1fbce7f981b347981d761efe1c98ce3f21549f32
ali-alzaiat <alialzaiat2000@gmail.com>
2023-10-28 15:31:56 +0200

    added chat input

--- chat-app/src/components/Input.jsx
+++ chat-app/src/components/Input.jsx
@@ -1,7 +1,8 @@
 function Input() {
     return ( 
         <div className="Input">
-            
+            <input className="messageInput" type="text" name="messageInput" id="messageInput" placeholder="Type something..." />
+            <button className="send">Send</button>
         </div>
      );
 }
