c7dfd011f73c06f2bc1a4018d3e275e142f90125
ali-alzaiat <alialzaiat2000@gmail.com>
2023-11-26 15:54:27 +0200

    added routes

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -2,10 +2,27 @@ import './App.css';
 import Register from './sites/register';
 import Login from './sites/login';
 import Main from './sites/Main';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "register",
+      element: <Register />,
+    },
+  ]);
+
   return (
-    <Main />
+    <RouterProvider router={router} />
   );
 }
 
