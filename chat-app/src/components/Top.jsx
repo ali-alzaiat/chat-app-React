2d7a418ad5e0f0aeda603a65e339ddf5c114f5df
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-17 21:24:16 +0200

    handled logout

--- chat-app/src/components/Top.jsx
+++ chat-app/src/components/Top.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function Top() {
+    let navigate = useNavigate();
+    function logoutHandler(){
+        localStorage.removeItem("token");
+        navigate("login");
+    }
     return ( 
         <div className="top">
             <div className="title">Chat</div>
             <div className="user">
                 <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
                 <span>Ali Alzaiat</span>
-                <button>logout</button>
+                <button onClick={logoutHandler}>logout</button>
             </div>
         </div>
      );
