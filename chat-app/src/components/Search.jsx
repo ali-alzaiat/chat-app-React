8013cd846e56c38b6a64a31c8e087ee07b29257d
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-20 19:37:34 +0200

    added receiver context

--- chat-app/src/components/Search.jsx
+++ chat-app/src/components/Search.jsx
@@ -1,12 +1,19 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../shared/context";
 
 function Search() {
     let [searchValue,setSearchValue] = useState("");
     let [searchResult,setSearchResult] = useState([]);
+    let {setReceiver,receiver} = useContext(UserContext);
     let searchHandler = (e)=>{
         setSearchValue(e.target.value);
     }
+    let recevierHandler = (e)=>{
+        const selectedUser = e.currentTarget.getAttribute('value');
+        setReceiver(selectedUser);
+    }
+    useEffect(()=>{console.log(receiver);},[receiver])
     useEffect(()=>{
         axios.get(`http://localhost:8000/user/getUser/${searchValue}`)
         .then((response)=>{
@@ -22,7 +29,7 @@ function Search() {
                 <input type="text" name="search" id="search" placeholder="Search for a user" onChange={searchHandler}/>
             </div>
             {searchResult.map((v)=>{
-                return (<div className="result">
+                return (<div className="result" key={v.name} value={v.name} onClick={recevierHandler}>
                 <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
                 <span className="userName">{v.name}</span>
             </div>)
--- chat-app/src/shared/context.jsx
+++ chat-app/src/shared/context.jsx
@@ -4,9 +4,10 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [username, setUsername] = useState('');
+  const [receiver, setReceiver] = useState('');
 
   return (
-    <UserContext.Provider value={{ username, setUsername }}>
+    <UserContext.Provider value={{ username, setUsername, receiver ,setReceiver }}>
       {children}
     </UserContext.Provider>
   );
