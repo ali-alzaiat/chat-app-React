929b639db63575b3e8f95ac72e84070e503fb5b0
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-20 19:19:34 +0200

    added search function

--- chat-app/src/components/Search.jsx
+++ chat-app/src/components/Search.jsx
@@ -1,13 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Search() {
+    let [searchValue,setSearchValue] = useState("");
+    let [searchResult,setSearchResult] = useState([]);
+    let searchHandler = (e)=>{
+        setSearchValue(e.target.value);
+    }
+    useEffect(()=>{
+        axios.get(`http://localhost:8000/user/getUser/${searchValue}`)
+        .then((response)=>{
+            setSearchResult(response.data);
+        }).catch((err)=>{
+            setSearchResult([]);
+            console.log(err)
+        })
+    },[searchValue])
     return ( 
         <div className="search">
             <div className="input-form">
-                <input type="text" name="search" id="search" placeholder="Search for a user" />
+                <input type="text" name="search" id="search" placeholder="Search for a user" onChange={searchHandler}/>
             </div>
-            <div className="result">
+            {searchResult.map((v)=>{
+                return (<div className="result">
                 <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
-                <span className="userName">Ali</span>
-            </div>
+                <span className="userName">{v.name}</span>
+            </div>)
+            })}
         </div>
      );
 }
