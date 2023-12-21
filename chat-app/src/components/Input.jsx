9a4f1638b3de21f6f7a9bb00b01961dcedd5b80d
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-21 18:39:17 +0200

    send new messages to database

--- chat-app/src/components/Input.jsx
+++ chat-app/src/components/Input.jsx
@@ -1,8 +1,32 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../shared/context";
+import axios from "axios";
+
 function Input() {
+    let {setMessageContent,email,receiver} = useContext(UserContext);
+    let [input,setInput] = useState('');
+    let token = localStorage.getItem("token");
+
+    let sendHandler = (e)=>{
+        setMessageContent(input)
+        axios.post("http://localhost:8000/messages/addMessage",{sender:email,receiver:receiver,text:input},{
+            headers:{
+                Authorization:`Basic ${token}`
+            }
+        }).catch((err)=>{
+            console.log(err);
+        })
+        document.getElementById('messageInput').value = ''
+    }
+
+    let inputHandler = (e)=>{
+        setInput(e.target.value)
+    }
+
     return ( 
         <div className="Input">
-            <input className="messageInput" type="text" name="messageInput" id="messageInput" placeholder="Type something..." />
-            <button className="send">Send</button>
+            <input className="messageInput" type="text" name="messageInput" id="messageInput" placeholder="Type something..." onChange={inputHandler} />
+            <button className="send" onClick={sendHandler}>Send</button>
         </div>
      );
 }
--- chat-app/src/shared/context.jsx
+++ chat-app/src/shared/context.jsx
@@ -6,9 +6,10 @@ export const UserProvider = ({ children }) => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [receiver, setReceiver] = useState('');
+  const [messageContent, setMessageContent] = useState('');
 
   return (
-    <UserContext.Provider value={{ username, setUsername, receiver ,setReceiver ,email ,setEmail}}>
+    <UserContext.Provider value={{ username, setUsername, receiver ,setReceiver ,email ,setEmail ,messageContent ,setMessageContent}}>
       {children}
     </UserContext.Provider>
   );
