431d21fe0a86a129253e7450a2276a8a881a7cb3
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-21 18:18:30 +0200

    display messages from database

--- chat-app/src/components/Message.jsx
+++ chat-app/src/components/Message.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
+import { UserContext } from "../shared/context";
+
 function Message(props) {
+    let time = props.message.timestamp.split('T')[1].substring(0,5);
+    let date = props.message.timestamp.split('T')[0]
+    let {username} = useContext(UserContext);
     return(
-        <div className={"message "+((props.owner)?'owner':'')}>
+        <div className={"message "+((props.message.sender.name === username)?'owner':'')}>
             <div className="messageInfo">
                 <img src="https://png.pngtree.com/png-vector/20191125/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_2035279.jpg" alt="" />
-                <span>just now</span>
+                <span>{time}</span>
             </div>
             <div className="messageContent">
-                <span>hello hi hih ihi hi hi hh  k nams asakdlk</span>
+                <span>{(props.message.sender.name !== username)?props.message.sender.name+': ':''}{props.message.content}</span>
             </div>
         </div>
     );
--- chat-app/src/components/Messages.jsx
+++ chat-app/src/components/Messages.jsx
@@ -1,14 +1,30 @@
+import { useContext, useEffect, useState } from "react";
 import Message from "./Message";
+import axios from "axios";
+import { UserContext } from "../shared/context";
 
 function Messages() {
+    let {username,receiver} = useContext(UserContext);
+    let [messageslist,setMessageslist] = useState([]);
+    useEffect(()=>{
+        let token = localStorage.getItem('token')
+        if(receiver){
+            axios.get(`http://localhost:8000/messages/getMessage/${username}/${receiver}`,{
+                headers:{
+                    Authorization:`Basic ${token}`
+                }
+            }).then((messages)=>{
+                setMessageslist(messages.data.messages);
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }
+    },[username,receiver])
     return ( 
         <div className="messages">
-            <Message owner="true" />
-            <Message />
-            <Message />
-            <Message owner="true" />
-            <Message />
-            <Message />
+            {messageslist.map((message)=>{
+                return (<Message message={message} key={message._id}/>)
+            })}
         </div>
      );
 }
