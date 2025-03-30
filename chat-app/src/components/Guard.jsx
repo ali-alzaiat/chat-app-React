9fe0f8021150cfd302909f589ab9624bb9732c98
ali-alzaiat <alialzaiat2000@gmail.com>
2025-03-30 14:02:24 +0200

    add loading indicator

--- chat-app/src/components/Guard.jsx
+++ chat-app/src/components/Guard.jsx
@@ -1,38 +1,44 @@
-import { useContext, useEffect, useState } from 'react';
-import { useNavigate, Outlet } from 'react-router-dom';
-import axios from 'axios';
-import { UserContext } from '../shared/context';
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "../shared/context";
 
 export default function Guard() {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const {setUsername,setEmail} = useContext(UserContext);
+  const { setUsername, setEmail } = useContext(UserContext);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     axios
-    .get('https://chat-backend-8dvr.onrender.com/user/verifyToken', {
-      headers: {
-        Authorization: `Basic ${token}`,
-      },
-    })
-    .then((response) => {
-      setUsername(response.data.name);
-      setEmail(response.data.email);
-      if (!response) {
-        navigate('/login');
-      } else {
-        setIsLoggedIn(true);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      navigate('/login');
-    });
-  }, [navigate,setUsername]);
+      .get("https://chat-backend-8dvr.onrender.com/user/verifyToken", {
+        headers: {
+          Authorization: `Basic ${token}`,
+        },
+      })
+      .then((response) => {
+        setUsername(response.data.name);
+        setEmail(response.data.email);
+        if (!response) {
+          navigate("/login");
+        } else {
+          setIsLoggedIn(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        navigate("/login");
+      });
+  }, [navigate, setUsername, setEmail]);
 
   if (!isLoggedIn) {
-    return null; // or any loading indicator or fallback component
+    return (
+      <div class="text-center">
+        <div class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+    ); // or any loading indicator or fallback component
   }
 
   return <Outlet />;
